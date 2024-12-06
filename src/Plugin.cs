d21479d873e5ddeece0ea3889f4b00a75b3157af
NBKRedSpy <none@example.com>
2024-12-06 18:40:36 -0500

    config update

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -15,14 +15,9 @@ namespace QM_CapAttack
     {
         public static string ModAssemblyName => Assembly.GetExecutingAssembly().GetName().Name;
 
-        public static string ModPersistenceFolder => Path.Combine(Application.persistentDataPath, ModAssemblyName);
-
         [Hook(ModHookType.AfterConfigsLoaded)]
         public static void AfterConfig(IModContext context)
         {
-            Directory.CreateDirectory(ModPersistenceFolder);
-
-
             new Harmony("NBKRedSpy_" + ModAssemblyName).PatchAll();
         }
 
